272e80c31cb0baa6796d80ca3dcbdc4d9672c9f3
Bhdrpkcn <bhdrpkcn@gmail.com>
2023-12-03 16:15:44 +0300

    before Favorited Items Display

--- src/components/body/BookCard.js
+++ src/components/body/BookCard.js
@@ -1,39 +1,73 @@
 import React from "react";
-import "./bookCard.css";
 import { useDispatch, useSelector } from "react-redux";
-import { addToFavorites } from "../../redux/reducers/bookSlice";
-import { Button } from "antd";
+import {
+  addToFavorites,
+  removeFromFavorites,
+} from "../../redux/reducers/bookSlice";
+import { Card, Badge } from "antd";
+import { SearchOutlined, PlusOutlined, MinusOutlined } from "@ant-design/icons";
 
 const BookCard = ({ book }) => {
   const dispatch = useDispatch();
-  const favoritedArray = useSelector(
-    (state) => state.books.favorited || []
-  );
+  const { Meta } = Card;
+  const favoritedArray = useSelector((state) => state.books.favorited || []);
   console.log("Favorited default:", favoritedArray);
 
   const handleAddToFavorite = () => {
-    if (!favoritedArray.some((favBook) => favBook.id === book.id)) {
-      dispatch(addToFavorites({ book }));
-      console.log("Favorited Array:", favoritedArray);
-    } else {
-      console.log("Book is already favorited");
-    }
+    dispatch(addToFavorites({ book }));
+  };
+  //not working FIX IT !
+  const handleClearFavorite = () => {
+    dispatch(removeFromFavorites());
   };
 
   return (
-    <div className="BookCard">
-      {book.image_url && (
-        <img className="bookImg" src={book.image_url} alt={book.title} />
-      )}
-      <div className="bookCred">
-        <div className="bookName"> {book.title} </div>
-        <div className="bookDetails">
-          <div className="bookAuthor">{book.authors}</div>
-          {book.rating}
-          <Button onClick={handleAddToFavorite}>Add</Button>
-        </div>
-      </div>
-    </div>
+    <>
+      <>
+        <Badge.Ribbon
+          style={{
+            margin: 15,
+            fontSize: 14,
+          }}
+          color="yellow"
+          text={book.rating}
+        >
+          <Card
+            style={{
+              width: 200,
+              height: 440,
+              margin: 15,
+              fontSize: 14,
+            }}
+            cover={
+              <img
+                style={{
+                  height: 300,
+                }}
+                alt={book.title}
+                src={book.image_url}
+              />
+            }
+            actions={[
+              <PlusOutlined onClick={handleAddToFavorite} />,
+              <MinusOutlined onClick={handleClearFavorite} />,
+              <SearchOutlined key="ellipsis" />,
+            ]}
+            hoverable={{
+              Boolean: true,
+            }}
+          >
+            <Meta
+              style={{
+                height: 50,
+              }}
+              title={book.title}
+              description={book.authors}
+            />
+          </Card>
+        </Badge.Ribbon>
+      </>
+    </>
   );
 };
 
--- src/components/body/BooksContainer.js
+++ src/components/body/BooksContainer.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchBooks } from "../../redux/reduxActions/booksActions";
 import BooksHeader from "./BooksHeader";
 import BookCard from "./BookCard";
+import { FloatButton } from "antd";
 
 const BooksContainer = () => {
   const dispatch = useDispatch();
@@ -24,14 +25,17 @@ const BooksContainer = () => {
   return (
     <div>
       <BooksHeader />
-      <div className="booksContainer">
-        {books.loading && <p>Loading...</p>}
-        {books.error && <p>Error: {books.error}</p>}
-        {!books.loading &&
-          !books.error &&
-          books.data.map((book, index) => (
-            <BookCard key={book.id || index} book={book} />
-          ))}
+      <div>
+        <div className="booksContainer">
+          {books.loading && <p>Loading...</p>}
+          {books.error && <p>Error: {books.error}</p>}
+          {<FloatButton.BackTop />}
+          {!books.loading &&
+            !books.error &&
+            books.data.map((book, index) => (
+              <BookCard key={book.id || index} book={book} />
+            ))}
+        </div>
       </div>
     </div>
   );
